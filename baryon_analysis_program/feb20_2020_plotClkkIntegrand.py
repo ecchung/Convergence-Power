25e33ddee098d4aeb1475d66b140248608418f99
Eegene Chung <echung@homard.cita.utoronto.ca>
2020-03-04 00:20:01 -0500

    feb28 edits to integrand difference plot

--- baryon_analysis_program/feb20_2020_plotClkkIntegrand.py
+++ baryon_analysis_program/feb20_2020_plotClkkIntegrand.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 from header.CAMB_header import *
 from header.import_baryon_data import import_data, interpolate_ratio
+from header.calculate_cl import P_delta, GetBaryonLensingPower
 import copy
 
 
@@ -104,52 +105,79 @@ a  = get_a(z)
 Wk = W(X, a, Xcmb)
 kl = (ell + 0.5)/X  # k at the given ell for each X
 
-# baryonic ones
+# load interpolators
 P_int_bary = dataset[which_sim]['P_interpolator'] # P_int(k, z); k and/or z can be a single value or a list
+R_int_bary = dataset[which_sim]['R_interpolator']
+R_int_DMO  = dataset[sim_DMO]['R_interpolator']. # literally just 1's
 
+''' as of feb 20 -- this one is wrong (feb 28)
+# baryonic ones
 Pl_bary    = np.diag(np.flip(P_int_bary(kl, z), axis=1))  
+R_int_bary = np.diag(np.flip(P_int(kl, z), axis=1))  
 # bad habit of resorting k's from lowest to highest, so I flip them
 # also takes each z for each k_l, but I only want pairs of k_l[i], z[i]
 I_bary     = (Wk/X)**2 * Pl_bary # Clkk integrand
 
 # DMO ones
 P_int_DMO = dataset[sim_DMO]['P_interpolator'] 
-
 Pl_DMO    = np.diag(np.flip(P_int_DMO(kl, z), axis=1))
 I_DMO     = (Wk/X)**2 * Pl_DMO 
+'''
 
-
-
-# interpolated data
-n   = 500
+# use interpolated data
+n   = 1000                                                # number of data points
 Xs  = np.linspace(0, Xcmb, n)[1:]
 zs  = results.redshift_at_comoving_radial_distance(Xs)
 As  = get_a(zs)
 Ws  = W(Xs, As, Xcmb)
 kls = (ell + 0.5)/Xs
 
-# baryonic ones
+'''
+# baryonic ones 
 Pls_bary    = np.diag(np.flip(P_int_bary(kls, zs), axis=1))  
 Is_bary     = (Ws/Xs)**2 * Pls_bary 
 
 # DMO ones
 Pls_DMO    = np.diag(np.flip(P_int_DMO(kls, zs), axis=1))
 Is_DMO     = (Ws/Xs)**2 * Pls_DMO 
+'''
 
+Is_bary = (Ws/Xs)**2 * P_delta(zs, kls, from_func='Weyl') * np.diagonal(np.flip(R_int_bary(kls, zs), axis=1))
+Is_DMO  = (Ws/Xs)**2 * P_delta(zs, kls, from_func='Weyl') #* np.diagonal(np.flip(R_int_DMO(kls, zs), axis=1)) #<-- just ones 
 
+'''
+    for j, datakey in enumerate(sim_datakey):
+        print(j, datakey)
+        P_ratio_int = sim_data[datakey]['R_interpolator']
+        cl_kappa_bary = np.zeros(lb.shape)
+        for i, li in enumerate(lb):
+            k = (li+0.5) / Xb
+            d[:] = 1
+            d[k>=kmax] = 0
+            cl_kappa_bary[i] = np.dot(dXb, d * P_delta(zb, k, from_func='Weyl') * np.diagonal(np.flip(P_ratio_int(k, zb), axis=1)) * wb**2 / Xb**2) 
+        cl_baryon_list.append(cl_kappa_bary)
+'''
 
 # plot
-fig, ax= plt.subplots(1, figsize=(8,6))
+'''
+#fig, ax= plt.subplots(1, figsize=(8,6))
 #ax.semilogx(zs[Xs>100],Pls_bary[Xs>100]/Pls_DMO[Xs>100], label='interpolated')
-ax.semilogx(zs,Pls_bary/Pls_DMO, label='interpolated')
-
+#ax.semilogx(zs,Pls_bary/Pls_DMO, label='interpolated')
 #ax.semilogx(z, Pl_bary/Pl_DMO,c='red',ls='None', marker='.',label='raw data') # <-- NOT RAW AT ALL. I USED THE INTERPOLATION
-plt.ylabel(r'${P_{\rm bary}(k_\ell,z)}$ / ${P_{\rm DMO}(k_\ell,z)}$', size=12)
-plt.xlabel(r'$z$')
+#plt.ylabel(r'${P_{\rm bary}(k_\ell,z)}$ / ${P_{\rm DMO}(k_\ell,z)}$', size=12)
+'''
+
+plt.figure(1, figsize=(8,6))
+plt.plot(Xs, Is_bary - Is_DMO, c='r', label='Difference between baryonic \nand DMO $C_\ell^{\kappa\kappa}$ integrand')
+plt.plot(X[-1]*np.ones(100), np.linspace(-4.5e-14, 0.2e-14, 100), c='k', ls='--', label='Max $\chi(z)$ of BAHAMAS-HighAGN \n'+r'($z_{max}=3$, $k_{max}=6508$)')
+plt.ylabel(r'Integrand of $C_{\ell \: \rm bary}^{\kappa\kappa}$ - Integrand of $C_{\ell \: \rm DMO}^{\kappa\kappa}$')
+plt.xlabel(r'$\chi(z)$')
 plt.title(r'BAHAMAS-HighAGN at $\ell=1000$')
-#plt.legend()
+plt.legend(frameon=False, loc='center right')
+plt.grid(True)
+plt.ylim(-4.5e-14, 0.2e-14)
 #plt.show()
-plt.savefig(savefolder + 'feb20_2020_P_ratio_l=1000_BAHAMAS_HighAGN.pdf')
+plt.savefig(savefolder + 'feb28_2020_Integrands_l=1000_BAHAMAS_HighAGN.pdf')
 
 
 
